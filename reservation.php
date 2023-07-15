d407820c015756f3557a4a7ddcf6af37f371581d
yel-x <132632053+yel-x@users.noreply.github.com>
2023-07-15 19:44:21 +0800

    Add files via upload

---
+++ reservation.php
@@ -0,0 +1,259 @@
+
+
+<!DOCTYPE html>
+<html>
+
+
+<head>
+<meta charset="UTF-8">
+<title>Villa Diwata Resort</title>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<!-- Linking Bootstrap css file -->
+<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+<!-- Linking Main Css file -->
+<link rel="stylesheet" type="text/css" href="css/animate.css">
+<link rel="stylesheet" type="text/css" href="css/font-awesome.min.css">
+<link rel="stylesheet" type="text/css" href="css/flatpickr.min.css">
+<link rel="stylesheet" type="text/css" href="css/owl.carousel.css">
+<link rel="stylesheet" type="text/css" href="css/style.css">
+<link rel="stylesheet" type="text/css" href="css/color.css">
+<link rel="stylesheet" type="text/css" href="css/responsive.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+	
+	<link rel="stylesheet" type="text/css" href="css/popup.php">
+</head>
+
+
+<body>
+
+	<div class="wrapper">
+
+
+	<header class="abs">
+			<div class="top-bar">
+				<div class="container">
+					<div class="con-links">
+						<ul>
+								<li><i class="fa fa-phone" aria-hidden="true"></i> (+63)9636342280</li>
+							<li><i class="fa fa-envelope" aria-hidden="true"></i>ACL@gmail.com</li>
+						</ul>
+					</div><!--con-links end-->
+					<div class="social-links">
+						<ul class="social-lnks">
+						<li><a href="#" title=""><i class="fa-brands fa-twitter"></i></a></li>
+						<li><a href="#" title=""><i class="fa-brands fa-facebook"></i></a></li>
+						<li><a href="#" title=""><i class="fa-brands fa-instagram"></i></a></li>
+						
+						
+						</ul>
+						
+					</div><!--socail-links end-->
+				</div>
+			</div><!--top-bar end-->
+			<div class="bottom-header">
+				<div class="container">
+					<div class="hd">
+						<div class="logo">
+							<a href="#" title="">
+								<img src="images/logo.jpg" alt="" />
+							</a>
+						</div><!--logo end-->
+						<div class="menu-search">
+							<nav>
+								<ul>
+									<li class="active menu-item-has-children"><a href="index.php" title=""><i class="fa-solid fa-house"></i>Home</a>
+										
+									</li>
+									
+									<li><a href="about.php" title="">About Us</a></li>
+									<li><a href="term.php" title="">Terms and Condition</a></li>
+								<li><a href="contact.php" title="">Contact Us</a></li>
+                                    <li><a href="reservation.php" title="">Reservation</a></li>
+
+								</ul>
+		 					</nav><!--nav end-->
+							
+						</div><!--menu-search end-->
+						<div class="menu-icon">
+							<span class="first-bar"></span>
+							<span class="second-bar"></span>
+							<span class="third-bar"></span>
+						</div>
+					</div>
+				</div>
+			</div><!--bottom-header end-->
+		</header><!--Header end-->
+	
+	
+<div class="about-bg">
+			<div class="container">
+				<div class="rl-banner">
+					<h2>Terms & Conditions</h2>
+					<ul></br>
+						<li><a href="#">Home</a></li>
+						<li><span class="active">Term & Conditions</span></li>
+					</ul>
+				</div>
+			</div>
+		</div>
+
+
+	
+	
+
+		<section>
+			<div class="block no-padding">
+				<div class="newsletter">
+					<div class="bg2">
+						<div class="container">
+							<div class="stay-tuned">
+								<h2>Stay tuned with us</h2>
+								<h5>Get our updated offers, discounts, events and much more!</h5>
+							</div>
+							<div class="email-form">
+								<form>
+									<input name="" placeholder="Enter your email address" type="text">
+									<button type="submit">Subscribe</button>
+								</form>
+							</div><!--email-form end-->
+						</div>
+					</div>
+				</div><!--newsletter end-->
+			</div>
+		</section>
+	
+	
+
+		<footer>
+			<div class="block no-padding">
+				<div class="bg bg3">
+					<div class="container">
+						<div class="top-footer">
+							<div class="row">
+								<div class="col-md-4">
+									<div class="widget">
+										<div class="about-widget">
+											<a href="#" title=""><img src="images/icon logo.jpg" alt=""></a>
+											<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit,  sed diam nonummy nibh euismod it tincidunt ut laoreet commodo consequat.  </p>
+										
+											<ul class="sc-links">
+											<li><a href="#" title=""><i class="fa-brands fa-twitter"></i></a></li>
+											<li><a href="#" title=""><i class="fa-brands fa-facebook"></i></a></li>
+											<li><a href="#" title=""><i class="fa-brands fa-instagram"></i></a></li>
+						
+											</ul>
+										</div><!--about-widget end-->
+									</div><!--widget end-->
+								</div>
+								<div class="col-md-3">
+									<div class="widget">
+										<div class="news-widget">
+											<h3 class="widget-title">Latest Rooms</h3>
+											<div class="news">
+												<img src="images/resources/sroom1.png" alt="">
+												<div class="news-details">
+													<h3><a href="#" title="">Single Room</a></h3>
+													<span>$85 night</span>
+												</div>
+											</div><!--news end-->
+											<div class="news">
+												<img src="images/resources/sroom2.png" alt="">
+												<div class="news-details">
+													<h3><a href="#" title="">Luxury Room</a></h3>
+													<span>$352 night</span>
+												</div>
+											</div><!--news end-->
+											<div class="news">
+												<img src="images/resources/sroom3.png" alt="">
+												<div class="news-details">
+													<h3><a href="#" title="">Double Room </a></h3>
+													<span>$125 night</span>
+												</div>
+											</div><!--news end-->
+										</div><!--news-widget end-->
+									</div><!--widget end-->
+								</div>
+								<div class="col-md-3">
+									<div class="widget">
+										<div class="widget-quick-links">
+											<h3 class="widget-title">Quick Links</h3>
+											<div class="qk-links">
+												<ul>
+													<li><a href="#" title="">Home</a></li>
+													<li><a href="#" title="">About Us</a></li>
+													<li><a href="#" title="">Suits</a></li>
+													<li><a href="#" title="">News</a></li>
+													<li><a href="#" title="">Contact Us</a></li>
+													<li><a href="#" title="">Bookisnt</a></li>
+													<li><a href="#" title="">Blogs</a></li>
+												</ul>
+												<ul>
+													<li><a href="#" title="">Activities</a></li>
+													<li><a href="#" title="">Gallery</a></li>
+													<li><a href="#" title="">Aminities</a></li>
+													<li><a href="#" title="">Single Room</a></li>
+													<li><a href="#" title="">Testimonials</a></li>
+													<li><a href="#" title="">Dinning</a></li>
+													<li><a href="#" title="">Offers</a></li>
+												</ul>
+											</div>
+										</div><!--widget-quick-links end-->
+									</div><!--widget-end-->
+								</div>
+								<div class="col-md-2 lst">
+									<div class="widget">
+										<div class="widget-tags">
+											<h3 class="widget-title">Tags</h3>
+											<ul>
+												<li><a href="#" title="">Booking</a></li>
+												<li><a href="#" title="">TV</a></li>
+												<li><a href="#" title="">Services</a></li>
+												<li><a href="#" title="">Room</a></li>
+												<li><a href="#" title="">Hostel</a></li>
+												<li><a href="#" title="">WIFI</a></li>
+												<li><a href="#" title="">AC</a></li>
+												<li><a href="#" title="">Camera</a></li>
+												<li><a href="#" title="">Party</a></li>
+												<li><a href="#" title="">Pool</a></li>
+												<li><a href="#" title="">PHP</a></li>
+												<li><a href="#" title="">Offers</a></li>
+												<li><a href="#" title="">Khignt</a></li>
+											</ul>
+										</div><!--widget-tags end-->
+									</div><!--widget end-->
+								</div>
+							</div>
+						</div><!--top-footer end-->
+						<div class="bottom-footer">
+							<div class="copyright">
+								<p>THANKYOU EVERYONE</p>
+							</div><!--copyright end-->
+							<div class="credit">
+								<p>Project of : <a href="#" title>Ariel Nazareno, Christian Almazan, LLoyd Hontiveros</a></p>
+							</div><!--credit end-->
+						</div><!--bottom-footer end-->
+					</div>
+				</div>
+			</div>
+		</footer><!--footer end-->
+
+
+
+
+
+	</div><!--wrapper end-->
+
+<!-- Including Jquery Js File -->
+<script type="text/javascript" src="http://creativethemes.us/relax/js/jquery.min.js"></script>
+<!-- Including Bootstrap js file -->
+<script type="text/javascript" src="http://creativethemes.us/relax/js/bootstrap.min.js"></script>
+<!-- Custom Js file -->
+<script type="text/javascript" src="http://creativethemes.us/relax/js/script.js"></script>
+
+
+</body>
+
+
+<!-- Mirrored from creativethemes.us/relax/reservation.html by HTTrack Website Copier/3.x [XR&CO'2014], Thu, 21 Sep 2017 15:23:47 GMT -->
+</html>
